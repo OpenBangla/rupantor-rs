88165a3893b581c2a29ef4572b6af03dfb5c14cb
Muhammad Mominul Huque <mominul2082@gmail.com>
2017-07-18 17:27:07 +0600

    Update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # bengali-phonetic-parser-rs
 [![Build Status](https://travis-ci.org/OpenBangla/bengali-phonetic-parser-rs.svg?branch=master)](https://travis-ci.org/OpenBangla/bengali-phonetic-parser-rs)
+
 A Bengali Phonetic Parser that obeys rules.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//extern crate json;
+
+pub struct PhoneticParser {
+    //patterns: json::Object,
+    vowel: String,
+    consonant: String,
+    numbers: String,
+    case_sensitive: String,
+}
+
+impl PhoneticParser {
+    fn is_vowel(&self, string: &str) -> bool {
+        self.vowel.contains(string)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
