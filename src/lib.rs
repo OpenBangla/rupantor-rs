743fbe16f769379f42ad8760e45d23787a441f36
Muhammad Mominul Huque <mominul2082@gmail.com>
2017-07-24 23:30:23 +0600

    Added new() function for loading rule file. Added test for helper functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
+#![allow(dead_code)]
 extern crate json;
 
-pub struct PhoneticParser {
-    patterns: json::JsonValue,
+pub struct PhoneticParser<'a> {
+    patterns: &'a json::JsonValue,
     vowel: String,
     consonant: String,
     numbers: String,
     case_sensitive: String,
 }
 
-impl PhoneticParser {
+impl<'a> PhoneticParser<'a> {
+    fn new(rule: &json::JsonValue) -> PhoneticParser {
+        PhoneticParser {
+            patterns: rule,
+            vowel: rule["vowel"].as_str().unwrap().to_string(),
+            consonant: rule["consonant"].as_str().unwrap().to_string(),
+            numbers: rule["number"].as_str().unwrap().to_string(),
+            case_sensitive: rule["casesensitive"].as_str().unwrap().to_string()
+        }
+    }
+    
     fn fix_string(&self, string: String) -> String {
         let mut fixed = String::new();
         for character in string.chars() {
@@ -22,11 +33,11 @@ impl PhoneticParser {
     }
 
     fn is_vowel(&self, string: char) -> bool {
-        self.vowel.contains(string)
+        self.vowel.contains(&string.to_lowercase().to_string())
     }
 
     fn is_consonant(&self, string: char) -> bool {
-        self.consonant.contains(string)
+        self.consonant.contains(&string.to_lowercase().to_string())
     }
 
     fn is_case_sensitive(&self, character: char) -> bool {
@@ -36,7 +47,34 @@ impl PhoneticParser {
 
 #[cfg(test)]
 mod tests {
+    use std::env;
+    use std::fs::File;
+    use std::io::Read;
+    use json;
+    use super::PhoneticParser;
+
+    fn get_rule() -> json::JsonValue {
+        // Get the rule file
+        let mut p = env::current_dir().unwrap();
+        p.push("AvroPhonetic.json");
+        let path = p.to_str().unwrap();
+        
+        let mut grammer = String::new();
+
+        let _ = File::open(path).unwrap().read_to_string(&mut grammer);
+
+        json::parse(&grammer).unwrap()
+    }
+    
     #[test]
-    fn it_works() {
+    fn test_helpers() {
+        let json = get_rule();
+        let parser = PhoneticParser::new(&json);
+
+        assert!(parser.is_vowel('A'));
+        assert_eq!(parser.is_vowel('b'), false);
+        assert!(parser.is_consonant('B'));
+        assert_eq!(parser.is_consonant('e'), false);
+        assert_eq!(parser.fix_string("ODEr AMAr".to_string()), "ODer amar");
     }
 }
