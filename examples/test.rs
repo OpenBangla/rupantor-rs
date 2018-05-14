0aa5ad26915301d820f5e4ac45b8737d952da25d
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-05-14 21:32:14 +0600

    Update example

--- examples/test.rs
+++ examples/test.rs
@@ -1,24 +1,11 @@
 extern crate bengali_phonetic_parser;
 extern crate json;
-use std::env;
-use std::fs::File;
-use std::io::Read;
 use bengali_phonetic_parser::PhoneticParser;
 
 fn main() {
-    // Get the rule file
-    let mut p = env::current_dir().unwrap();
-    p.push("AvroPhonetic.json");
-    let path = p.to_str().unwrap();
-        
-    let mut grammer = String::new();
-
-    let _ = File::open(path).unwrap().read_to_string(&mut grammer);
-    let js = json::parse(&grammer).unwrap();
+    // Parse the rule file
+    let js = json::parse(include_str!("../src/AvroPhonetic.json")).unwrap();
 
     let cvt = PhoneticParser::new(&js);
     println!("{}", cvt.convert("ami banglay gan gai".to_owned()));
-/*
-    let _find = &js["patterns"];
-    println!("{}", _find.len());*/
 }
